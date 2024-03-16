84e3f3067597aeeedc12a00bd3d3d759e00dbd6b
azfarsyed <122569999+azfarsyed@users.noreply.github.com>
2024-03-16 21:34:46 +0000

    Add files via upload

---
+++ board.c
@@ -0,0 +1,95 @@
+/**
+ * Assignment: life
+ * Name AZFAR SYED	
+ * PID: A17267531
+ * Class: UCSD CSE30-SP21
+ *
+ */
+#include "cse30life.h"
+#include "board.h"
+
+
+/**
+ * create a new board
+ *
+ * - malloc a boards structure
+ * - set the generation to 0
+ * - open the file (if it doesn't exist, return a NULL pointer
+ * - read the first line which is the number of rows
+ * - read the second line which is the number of cols
+ * - set the # of rows and # of cols in the boards structure
+ * - malloc bufferA and bufferB 
+ * - Set currentBuffer and nextBuffer
+ * - clear both board buffers
+ * - read the file until done.  each row contains a row and a columns separted by
+ *   white space
+ *     for each line, set the cell in the current buffer
+ * - close the file
+ * - return the boards pointer if successfull or NULL ptr otherwise
+ */
+boards_t * createBoard(char *initFileName){
+  FILE *fp; 
+  if((fp = fopen(initFileName, "r")) == NULL) { 
+    return NULL; 
+  }
+  boards_t *board = malloc(sizeof(boards_t));
+
+  fscanf(fp, "%zu", &board->numRows); 
+  fscanf(fp, "%zu",&board->numCols);
+
+  board->bufferA = malloc(board->numRows * board->numCols * sizeof(unsigned char));
+  board->bufferB = malloc(board->numRows * board->numCols * sizeof(unsigned char));
+  board->currentBuffer = board->bufferA;
+  board->nextBuffer = board->bufferB;
+
+  size_t numOne; 
+  size_t numTwo; 
+
+  clearBoards(board); 
+  while(fscanf(fp, "%zu %zu",&numOne, &numTwo) > 0) { 
+      size_t index = getIndex(board->numCols, numOne, numTwo);
+      board->currentBuffer[index] = 1;
+  }
+  board->gen = 0;
+  fclose(fp);
+  return board;  
+}
+
+/**
+ * delete a board
+ */
+void deleteBoard(boards_t **bptrPtr){
+   free((*bptrPtr)->bufferB);
+   free((*bptrPtr)->bufferA);
+   free(*bptrPtr); 
+   *bptrPtr = NULL;
+}
+
+/**
+ * set all the belems in both buffers to 0
+ */
+void clearBoards(boards_t *self){
+   size_t totalCells = self->numRows * self->numCols; 
+
+   for(int i = 0; i < totalCells;i++){ 
+	self->currentBuffer[i] = 0;
+        self->nextBuffer[i] = 0;
+   }
+}
+
+/**
+ * swap the current and next buffers
+ */
+void swapBuffers(boards_t *self){
+  belem *temp = self->currentBuffer; 
+  self->currentBuffer = self->nextBuffer; 
+  self->nextBuffer = temp; 
+}
+
+
+/**
+ * get a cell index
+ */
+size_t getIndex(size_t numCols, size_t row, size_t col){
+   return  numCols*row + col;
+}
---
+++ sim.c
@@ -0,0 +1,74 @@
+
+/**
+  * Assignment: life
+  * Name: Azfar Syed
+  * PID: A17267531
+  * Class: UCSD CSE30-SP21
+  */
+
+#include "sim.h"
+
+#define CIMP
+extern void asm_doRow(belem *, belem *, size_t, size_t, size_t);
+
+/**
+ * gets x mod N (works for negative numbers as well! Use this instead of %)
+ */
+size_t getModVal(int x, size_t N){
+    size_t adj = x/N;
+    return((x+adj*N)%N);
+}
+
+/**
+ * process one row of the board
+ */
+static void doRow(belem *dest, belem *src, size_t row, size_t rows, size_t cols){
+  for(size_t i = 0; i < cols; i++) {
+    int neighbors = 0; 
+    for(int j = -1; j <= 1; j++) { 
+      for(int k = -1; k <= 1; k++) { 
+        if(j == 0 && k == 0) { 
+          continue; 
+        } 
+        size_t adjRow = getModVal(row + j, rows); 
+        size_t adjCol = getModVal(i + k, cols); 
+        neighbors += src[getIndex(cols, adjRow, adjCol)]; 
+      }
+    } 
+    size_t index = getIndex(cols, row, i); 
+    if(src[index] == 1) { 
+      if(neighbors == 1 || neighbors == 0) { 
+        dest[index] = 0;
+      } else if(neighbors == 2 || neighbors == 3) {
+        dest[index] = 1;
+      } else if(neighbors ==4) {
+        dest[index] = 0;
+      }
+    } else {
+      if(neighbors == 3) {
+        dest[index] = 1;
+      } else {
+        dest[index] = 0;
+      }
+    }
+  }
+}
+
+
+/**
+ * perform a simulation for "steps" generations
+ *
+ * for steps
+ *   calculate the next board
+ *   swap current and next
+: */
+void simLoop(boards_t *self, unsigned int steps){
+  for(int i = 0; i < steps; i++) {
+    for(size_t j = 0; j < self->numRows; j++) {
+      doRow(self->nextBuffer, self->currentBuffer, j, self->numRows, self->numCols);
+    }
+    swapBuffers(self);
+    self->gen++;
+  }
+}
+
